8b58a3a5e4be242531a90d731d73e63c10fa2894
Joshua MacSween <joshmacsween@gmail.com>
2021-01-29 15:34:38 -0700

    init

---
+++ App.js
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
