ac9ccc24c08bad4a74c93ad4b8bdd614ecf82896
Joshua MacSween <joshmacsween@gmail.com>
2021-01-30 12:37:54 -0700

    working icon

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/Listitem';
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([
+    {id: 1, text: 'Milk'},
+    {id: 2, text: 'Eggs'},
+    {id: 3, text: 'Juice'},
+    {id: 4, text: 'Bread'},
+  ]);
 
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem key={item.id} item={item} />}
+      />
     </View>
   );
 };
---
+++ components/Listitem.js
@@ -0,0 +1,41 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faCoffee,
+  faBeer,
+  faCircle,
+  faCheck,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+
+const ListItem = ({item}) => {
+  return (
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{item.text}</Text>
+        <FontAwesomeIcon size={20} color="firebrick" icon={faTrash} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+});
+
+export default ListItem;
